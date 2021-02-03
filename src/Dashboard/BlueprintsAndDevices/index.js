9f9058fc8955bf109f8a4da584927a4de1ba548c
zephyrz73 <xzhou1551@outlook.com>
2021-02-02 20:28:20 -0800

    blue print

--- src/Dashboard/BlueprintsAndDevices/index.js
+++ src/Dashboard/BlueprintsAndDevices/index.js
@@ -1,9 +1,64 @@
-import React from 'react';
+import React, {useRef, useState, useEffect} from 'react'
+import example from "../assets/uploaded_blueprints/example.jpg"
+import mark from "../assets/UI_component/source 2.png"
 
-export default function BlueprintsAndDevices() {
+const list = [];
+
+const BlueprintsAndDevices = props => {
+  const [dotList, setDotList] = useState([]);
+  const canvasRef = useRef(null);
+
+  useEffect(() => {
+    const canvas = canvasRef.current;
+    const context = canvas.getContext('2d');
+    const img = document.getElementById("bp");
+    img.onload = function() {
+      context.drawImage(img, 0, 0, 800, 700);
+    };
+    img.src = example;
+    var dot = new Image();
+    dot.src = mark;
+    canvas.addEventListener("click", function(event){
+      var x = event.offsetX - 10;
+      var y = event.offsetY - 10;
+      context.drawImage(dot, x, y, 25, 25);
+    });
+  }, []);
+
+  function drawOne(event){
+    var dot = new Image();
+    dot.src = mark;
+    var x = event.nativeEvent.offsetX - 10;
+    var y = event.nativeEvent.offsetY - 10;
+    let each = {"x": x, "y": y};
+    list.push(each);
+    alert("A new device is placed");
+    console.log(dotList);
+    setDotList(list);
+  }
+
+
+  function handleImage(e){
+    let canvas = canvasRef.current;
+    let ctx = canvas.getContext('2d');
+    alert("New blueprint uploaded");
+    var reader = new FileReader();
+    reader.onload = function(event){
+        var img = new Image();
+        img.onload = function(){
+            ctx.drawImage(img,0,0, 800, 700);
+        }
+        img.src = event.target.result;
+    }
+    reader.readAsDataURL(e.target.files[0]);
+  }
+
   return (
-    <div>
-      <h1>Blueprints and Devices</h1>
-    </div>
-  );
+  <div>
+    <canvas id="board" ref={canvasRef} width="800px" height="700px" onClick={drawOne}/>
+    <img id="bp" src={example} alt="blueprint" style={{display: "none"}}/>
+    <input type="file" title="New Blueprint" id="imageLoader" name="imageLoader" onChange={handleImage}/>
+  </div>)
 }
+
+export default BlueprintsAndDevices;
---
+++ src/Dashboard/index.js
@@ -0,0 +1,100 @@
+import './dashboard.css';
+import './dashboard2.css';
+
+/* src/App.js */
+import React, { useEffect, useState } from 'react'
+import Amplify, { API, graphqlOperation } from 'aws-amplify'
+import { getDeviceDataTest } from '../graphql/queries'
+
+/* frontend-imports */
+import { Button, Container, Row, Col, Card, Tab, Tabs } from 'react-bootstrap';
+import { Navbar, Nav, NavDropdown} from 'react-bootstrap';
+import { Form, FormControl} from 'react-bootstrap';
+import logo from './assets/favicon.svg';
+import Slider from './assets/Slider.js';
+
+
+
+const initialState = { id: '', time: '', sensor: '', timestamp: ''}
+
+const Dashboard = () => {
+  const [formState, setFormState] = useState(initialState)
+
+  async function displayDevice() {
+    try {
+      const store = await API.graphql({ query: getDeviceDataTest, variables: {id: '02f9bde0-197d-11eb-8848-d19ed1c9f7eb'} });
+      const info = store.data.getDeviceDataTest;
+      const payload = JSON.parse(info.payload);
+      const newData = {id: info.id, time: payload['time'], sensor: 'a'+payload['sensor_a0'], timestamp: info.timestamp};
+      setFormState(newData);
+      console.log(newData);
+      console.log(payload);
+    } catch (err) {
+      console.log('error: ', err);
+    }
+  }
+
+  return (
+    <Container fluid>
+      <Navbar bg="light">
+        <Navbar.Brand href="#home">React-Bootstrap</Navbar.Brand>
+        <Navbar.Toggle aria-controls="basic-navbar-nav" />
+        <Navbar.Collapse id="basic-navbar-nav">
+        <Form inline>
+          <FormControl type="text" placeholder="Suggestions" className="mr-sm-2" />
+        </Form>
+        </Navbar.Collapse>
+      </Navbar>
+      <Row>
+        <Col xs={2}>
+          <Nav defaultActiveKey="/home" className="flex-column">
+            <Nav.Link href="/home">Home</Nav.Link>
+            <Nav.Link eventKey="link-1">Alert</Nav.Link>
+            <Nav.Link eventKey="link-2">Blueprints and Device</Nav.Link>
+            <Nav.Link eventKey="link-3">Settings</Nav.Link>
+          </Nav>
+        </Col>
+        <Col xs={6}>
+        <Tabs defaultActiveKey="profile" id="uncontrolled-tab-example">
+          <Tab eventKey="home" title="Nano">
+            <p>Nano</p>
+            <Slider></Slider>
+          </Tab>
+          <Tab eventKey="profile" title="Gas">
+            <p>Gas</p>
+          </Tab>
+          <Tab eventKey="contact" title="Temperature" >
+            <p>Temprature</p>
+          </Tab>
+        </Tabs>
+        </Col>
+        <Col xs={4}>
+          <Row>
+            <Col>
+            <p>My Devices</p>
+            </Col>
+            <Col>
+            <Button onClick={displayDevice}>Add</Button>
+            </Col>
+          </Row>
+          <Row>
+          <Card style={{ width: '18rem' }}>
+            <Card.Img variant="top" />
+            <Card.Body>
+              <Card.Title>id: {formState.id}</Card.Title>
+              <Card.Text>
+                <div>time: {formState.time}</div>
+                <div>sensor: {formState.sensor}</div>
+                <div>timestamp: {formState.timestamp}</div>
+              </Card.Text>
+              <Button variant="primary">Delete</Button>
+            </Card.Body>
+          </Card>
+          </Row>
+
+        </Col>
+      </Row>
+    </Container>
+  )
+}
+export default Dashboard;
