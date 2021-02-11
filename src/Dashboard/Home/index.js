8ed9cbc7cd97293d04157cb727604c44e1a51d1a
zephyrz73 <xzhou1551@outlook.com>
2021-02-11 00:22:07 -0800

    home scroll bar

--- src/Dashboard/Home/index.js
+++ src/Dashboard/Home/index.js
@@ -5,80 +5,38 @@
 import React, { useEffect, useState } from 'react'
 import Amplify, { API, graphqlOperation } from 'aws-amplify'
 import { getMockDeviceDataTest } from '../../graphql/queries'
+import examplePic from '../assets/uploaded_blueprints/example.jpg'
+import './home.css'
 
 /* frontend-imports */
-import { Button, Container, Row, Col, Card, Tab, Tabs } from 'react-bootstrap';
-import { Navbar, Nav, NavDropdown} from 'react-bootstrap';
-import { Form, FormControl} from 'react-bootstrap';
+
 import logo from '../assets/favicon.svg';
 import Slider from '../assets/Slider.js';
-
-
+import Paper from '@material-ui/core/Paper';
+import Box from '@material-ui/core/Box';
+import Tabs from '@material-ui/core/Tabs';
+import Tab from '@material-ui/core/Tab';
+import AppBar from '@material-ui/core/AppBar';
 
 const initialState = { id: '', time: '', sensor: '', timestamp: ''}
 
 const Dashboard = () => {
   const [formState, setFormState] = useState(initialState)
 
-  async function displayDevice() {
-    try {
-      const store = await API.graphql({ query: getMockDeviceDataTest, variables: {sensor_id: '18', timestamp: '1606880248914'} });
-      const info = store.data.getMockDeviceDataTest;
-      console.log(info);
-      const newData = {sensor_id: info.sensor_id, timestamp: info.timestamp, Dp: info.Dp_greater_point3, latitude: info.latitude, device_time: info.device_time, longitude: info.longitude};
-      console.log(newData);
-      setFormState(newData);
-    }catch (err) {
-      console.log('error: ', err);
-    }
-  }
-
   return (
-    <Container fluid>
-      <Row>
-        <Col xs={6}>
-        <Tabs defaultActiveKey="profile" id="uncontrolled-tab-example">
-          <Tab eventKey="home" title="Nano">
-            <p>Nano</p>
-            <Slider></Slider>
-          </Tab>
-          <Tab eventKey="profile" title="Gas">
-            <p>Gas</p>
-          </Tab>
-          <Tab eventKey="contact" title="Temperature" >
-            <p>Temprature</p>
-          </Tab>
-        </Tabs>
-        </Col>
-        <Col xs={4}>
-          <Row>
-            <Col>
-            <p>My Devices</p>
-            </Col>
-            <Col>
-            <Button onClick={displayDevice}>Add</Button>
-            </Col>
-          </Row>
-          <Row>
-          <Card style={{ width: '18rem' }}>
-            <Card.Img variant="top" />
-            <Card.Body>
-              <Card.Title>sensor id: {formState.sensor_id}</Card.Title>
-              <Card.Text>
-                <div>timestamp: {formState.timestamp}</div>
-                <div>Dp: {formState.Dp}</div>
-                <div>latitude: {formState.latitude}</div>
-                <div>device time: {formState.device_time}</div>
-                <div>longitude: {formState.longitude}</div>
-              </Card.Text>
-              <Button variant="primary">Delete</Button>
-            </Card.Body>
-          </Card>
-          </Row>
+    <div id="home-main">
+      <AppBar position="static">
+      <Tabs id="type-sensor-tab" style={{display: 'flex', justifyContent: 'center'}}>
+        <Tab label="Nano Particle"/>
+        <Tab label="Gas"/>
+        <Tab label="Temperature" />
+      </Tabs>
+      </AppBar>
+      <Slider>
+      </Slider>
+      <img src={examplePic}></img>
+    </div>
 
-        </Col>
-      </Row>
-    </Container>
   )
 }
 export default Dashboard;
--- src/Dashboard/assets/Slider.js
+++ src/Dashboard/assets/Slider.js
@@ -5,42 +5,68 @@ import Slider from '@material-ui/core/Slider';
 
 const useStyles = makeStyles((theme) => ({
   root: {
-    width: 250,
+    width: '90%',
   },
   margin: {
     height: theme.spacing(3),
   },
 }));
 
-const marks = [
-  {
-    value: 40,
-    label: '1:00pm',
-  },
-  {
-    value: 10,
-    label: '2:00pm',
-  }
-];
+// const marks = [
+//   {
+//     value: 40,
+//     label: '1:00pm',
+//   },
+//   {
+//     value: 10,
+//     label: '2:00pm',
+//   }
+// ];
 
-function valuetext(value) {
-  return `${value}°C`;
+const marks = () => {
+  var marks = [];
+  for (let i = 0; i <= 24; i++) {
+    var label = '';
+    if (i == 0) {
+      label = '12';
+    }
+    if (i > 0 && i < 10) {
+      label = '0'+i;
+    }
+    if (i >= 10 && i <= 12) {
+      label = i;
+    }
+    if (i > 12 && i < 22) {
+      label = "0"+(i-12);
+    }
+    if (i >= 22) {
+      label = i-12;
+    }
+    marks.push({
+      value: i,
+      label: label
+    })
+  }
+  return marks;
 }
+
 
 export default function TrackFalseSlider() {
   const classes = useStyles();
 
   return (
-    <div className={classes.root}>
+    <div className={classes.root} style={{margin: "3vh"}}>
       <Typography id="track-false-range-slider" gutterBottom>
-        Time
+        Scroll to view sensor status throughout the day
       </Typography>
       <Slider
-        track={false}
-        aria-labelledby="track-false-range-slider"
-        getAriaValueText={valuetext}
-        defaultValue={[20, 37, 50]}
-        marks={marks}
+        defaultValue={30}
+        aria-labelledby="slider-time"
+        step={1}
+        marks={marks()}
+        valueLabelDisplay="off"
+        min={0}
+        max={24}
       />
     </div>
   );
